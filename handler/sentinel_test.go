6a43dcf5e3496bdbad4acd80fb613ba325506b4d
opan <opan.neutron@gmail.com>
2023-11-21 17:01:29 +0700

    fix: add test to GetSentinelHandller

--- handler/sentinel_test.go
+++ handler/sentinel_test.go
@@ -3,11 +3,14 @@ package handler_test
 import (
 	"bytes"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/sentinel-dashboard/db"
 	"github.com/sentinel-dashboard/handler"
+	"github.com/sentinel-dashboard/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,4 +34,70 @@ func Test_registerSentinelHandler(t *testing.T) {
 	respData, _ := io.ReadAll(w.Body)
 	assert.Equal(t, http.StatusCreated, w.Code)
 	assert.Equal(t, `{"errors":[],"msg":"Sentinel successfully registered"}`, string(respData))
+}
+
+func Test_getSentinelHandler(t *testing.T) {
+	dbConn := setupTest()
+	defer dbConn.Close()
+
+	setupDummySentinelHandler(dbConn)
+
+	req, _ := http.NewRequest("GET", "/sentinel", nil)
+	h := handler.New(dbConn)
+	r := h.Router()
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	respData, _ := io.ReadAll(w.Body)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"errors":[],"msg":"","data":[]}`, string(respData))
+
+	// db := dbConn.GetConnection()
+	// rows, err := db.Query("SELECT COUNT(id) FROM sentinels")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// defer rows.Close()
+	// var i int
+	// for rows.Next() {
+
+	// 	_ = rows.Scan(&i)
+	// }
+
+	// fmt.Println(i)
+
+}
+
+func setupDummySentinelHandler(dbConn db.DB) {
+	db := dbConn.GetConnection()
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare(`INSERT INTO sentinels (name, hosts) VALUES (?, ?)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	sentinelTables := []model.Sentinel{
+		{Name: "sentinel-dummy-1", Hosts: "10.12.1.1:26379"},
+		{Name: "sentinel-dummy-2", Hosts: "10.12.1.1:26379"},
+	}
+
+	for _, v := range sentinelTables {
+		_, err = stmt.Exec(v.Name, v.Hosts)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
