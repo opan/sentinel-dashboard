730455fe7c74424c76734868b88ab88223072076
opan <opan.neutron@gmail.com>
2024-04-06 16:24:17 +0700

    fix: add db struct tag to created_at field in sentinel model

--- model/sentinel.go
+++ model/sentinel.go
@@ -6,5 +6,5 @@ type Sentinel struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Hosts     string    `json:"hosts"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
