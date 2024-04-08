99a9ca3de867c17ce25e64bf9e3a05b4bfd5d781
opan <opan.neutron@gmail.com>
2024-04-09 06:51:51 +0700

    fix: try to add graceful shutdown in the server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
@@ -17,9 +22,11 @@ type Sentinels struct {
 }
 
 func main() {
+	ctx := context.TODO()
+
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file: %s\n", err)
 	}
 
 	dbConn := db.New(os.Getenv("DB_FILE_NAME"))
@@ -28,8 +35,35 @@ func main() {
 	fmt.Println("Run DB Migration")
 	dbConn.Migrate()
 
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	fmt.Println("Starting Sentinel Manager Server")
 	h := handler.New(dbConn)
-	r := h.Router()
-	r.Run(":" + os.Getenv("BACKEND_PORT"))
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", os.Getenv("BACKEND_PORT")),
+		Handler: h.Router(),
+	}
+
+	// init the server in goroutine so that it won't block the graceful shutdown handling below
+	go func() {
+		if err = srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// listen for the interrupt signal
+	<-ctx.Done()
+
+	// restore default behaviour on the interrupt signal and notify user of shutdown
+	stop()
+	log.Println("shutting down gracefully, press ctrl+C to force shutdown")
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	if err = srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server gracefully shutdown. Exiting")
 }
