b5442bc0245c0084931a2bc5792a7a0eb7709aca
opan <opan.neutron@gmail.com>
2024-04-13 17:54:09 +0700

    fix: add to execute cmd from sentinel client

--- handler/cluster.go
+++ handler/cluster.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -34,20 +35,49 @@ func (h *handler) ClusterInfoHandler() gin.HandlerFunc {
 			return
 		}
 
-		sentinel := redis.NewSentinelClient(&redis.Options{})
-		defer sentinel.Close()
-
 		// set 5s timeout when executing
 		ctxTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
 		defer cancel()
 
-		ping, err := sentinel.Ping(ctxTimeout).Result()
-		if err != nil {
-			ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("failed ping: %w", err))
+		var sentinel *redis.SentinelClient
+		var okHost string
+
+		// parse sentinel hosts
+		// use the first healthy sentinel host found
+		// otherwise, raise error if not found
+		sh := strings.Split(s.Hosts, ",")
+		for _, v := range sh {
+			sentinel = redis.NewSentinelClient(&redis.Options{
+				Addr: v,
+			})
+
+			ping, err := sentinel.Ping(ctxTimeout).Result()
+			if err == nil && ping == "PONG" {
+				okHost = v
+				break
+			} else {
+				sentinel.Close()
+			}
 		}
 
+		if okHost == "" {
+			ctx.JSON(http.StatusServiceUnavailable, fmt.Errorf("No successfull ping to all available sentinel hosts: %s", s.Hosts))
+			return
+		}
+
+		cmd := redis.NewCmd(ctx, "info")
+
+		err = sentinel.Process(ctx, cmd)
+		if err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("error processing command: %w", err))
+			return
+		}
+		defer sentinel.Close()
+
+		fmt.Println(cmd.Result())
+
 		ctx.JSON(http.StatusOK, gin.H{
-			"data":   ping,
+			"data":   "",
 			"errors": []string{},
 		})
 	}
