b56035fe1662ee330a8d26899e594759c8114b0b
opan <opan.neutron@gmail.com>
2024-04-28 16:49:59 +0700

    chore: push just so i have records of initial plan for ui

--- ui/sentinel-manager-ui/src/app/layout.tsx
+++ ui/sentinel-manager-ui/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,52 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <main className="min-h-screen flex flex-row flex-auto flex-shrink-0 bg-gray-50 text-black font-bold">
+          <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
+            <div className="flex items-center justify-center h-14 border-b">
+              <div className='text-xl'>Sentinel Manager</div>
+            </div>
+
+            <div className="overflow-y-auto overflow-x-hidden flex-grow">
+              <ul className="flex flex-col py-4 space-y-1">
+                <li className="px-5">
+                  <div className="flex flex-row items-center h-8">
+                    <div className="text-sm font-light tracking-wide text-gray-500">Menu</div>
+                  </div>
+                </li>
+                <li></li>
+                <li>
+                  <Link href={"#"} className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                    <span className="inline-flex justify-center items-center ml-4">
+                      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round"  strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+
+                    </span>
+                    <span className="ml-2 text-sm tracking-wide truncat">Dashboard</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"#"} className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                    <span className="inline-flex justify-center items-center ml-4">
+                      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round"  strokeLinejoin="round" strokeWidth="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
+                    </span>
+                    <span className="ml-2 text-sm tracking-wide truncat">Profile</span>
+                  </Link>
+                </li>
+                <li className="px-5">
+                  <div className="flex flex-row items-center h-8">
+                    <div className="text-sm font-light tracking-wide text-gray-500">Tasks</div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className=''>
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
